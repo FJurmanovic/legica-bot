972a0e370d4dd7382fd887028f89fc9169870307
Fran Jurmanović <fjurma12@outlook.com>
2023-09-29 22:10:32 +0200

    use .env

--- app.ts
+++ app.ts
@@ -3,13 +3,8 @@ import Chat from "./common/chat";
 import { config as dotenv } from "dotenv";
 import { Controller } from "./core";
 import { ClientController } from "./controllers";
-import { ENVIRONMENT } from "./constants";
-import { getSettings } from "./common";
 
 dotenv();
-
-const environment: string = ENVIRONMENT;
-const appSettings: any = getSettings(environment);
 
 const client: Client = new Client();
 const chat: Chat = new Chat(client);
@@ -17,4 +12,4 @@ const chat: Chat = new Chat(client);
 const controllers = new Controller(new ClientController(client));
 
 controllers.register();
-chat.register(appSettings?.token || process.env.DEV_TOKEN || "");
+chat.register(process.env.DEV_TOKEN || "");
