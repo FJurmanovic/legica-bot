71c9d1635b1bc560d73c883f7ab52d9cb58a5180
Fran Jurmanović <fjurma12@outlook.com>
2023-10-05 19:05:33 +0200

    use constant url

--- src/common/getFirstHtml.ts
+++ src/common/getFirstHtml.ts
@@ -1,8 +1,9 @@
+import { config } from "@constants";
 import axios from "axios";
 import cheerio from "cheerio";
 
 export async function getFirstHtml(): Promise<string> {
-	const response = await axios.get("https://sib.net.hr/legica-dana");
+	const response = await axios.get(config.LEGICA_URL);
 	const html = response.data;
 	const $ = cheerio.load(html);
 	const { href } = $(".News-link.c-def")?.attr() || {};
